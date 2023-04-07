517d8249f8195a2b210f90cacf2cde063abf5055
ginohmk <michotall95@gmail.com>
2023-04-07 20:36:15 +0000

    add: Add middleware express urlEncodeer for receiveing form data extended: true

--- render-asset/server.js
+++ render-asset/server.js
@@ -6,42 +6,48 @@ app = express();
 // parseing Data
 app.use(express.json());
 
+// configure to parse data from form
+app.use(express.urlencoded({ extended: true }));
+
 //Listen to port
 app.listen(9000, () => console.log('Server Connected!!!'));
+
+/** Server static file
+ *  From public folder */
+
+// Use a middleware
+app.use(express.static('public'));
 
 /**
  *
  * ROUTE
  */
 
+const rootFolder = __dirname;
+
 //--- Home
 app.get('/', (req, res) => {
-  res.json({
-    statusCode: 200,
-    message: 'Welcome home',
-  });
+  res.sendFile(rootFolder + '/public/pages/index.html');
 });
 
 //--- About
 app.get('/about', (req, res) => {
-  res.json({
-    statusCode: 200,
-    message: 'Welcome About',
-  });
+  res.sendFile(rootFolder + '/public/pages/about.html');
 });
 
 //--- Add post
-app.post('/posts', (req, res) => {
-  res.json({
-    statusCode: 200,
-    message: 'Welcome Post Add',
-  });
+app.get('/addPost', (req, res) => {
+  res.sendFile(rootFolder + '/public/pages/addPost.html');
 });
 
 //--- Get post form
-app.get('/posts', (req, res) => {
+app.post('/addPost', (req, res) => {
+  console.log('In podt herer');
+
+  const { title, url, body } = req.body;
+
   res.json({
     statusCode: 200,
-    message: 'Welcome Post form',
+    message: `${title} ${url} ${body}`,
   });
 });
