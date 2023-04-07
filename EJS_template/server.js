09a461d7b9f7153cf43f8c9f93833d9098b45948
ginohmk <michotall95@gmail.com>
2023-04-07 21:07:11 +0000

    Add: injection of data with ejs

--- EJS_template/server.js
+++ EJS_template/server.js
@@ -2,8 +2,11 @@ const express = require('express');
 
 app = express();
 
+// set view engine
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-  res.send('Welcome');
+  res.render('index.ejs');
 });
 
 app.listen(9000, () => console.log('Server Connected'));
