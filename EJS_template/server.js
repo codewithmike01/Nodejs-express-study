29f6e09ce5ede6c4c0bbe1fc761a5f5a9df536a0
ginohmk <michotall95@gmail.com>
2023-04-08 07:55:52 +0000

    add: Add dynamic render from server

--- EJS_template/server.js
+++ EJS_template/server.js
@@ -12,8 +12,18 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   res.render('aboutUs.ejs');
 });
+
 app.get('/contact', (req, res) => {
   res.render('contact');
+});
+
+app.get('/profile', (req, res) => {
+  const user = {
+    name: 'Mike',
+    age: 20,
+    city: 'Lagos',
+  };
+  res.render('profile', { user });
 });
 
 app.listen(9000, () => console.log('Server Connected'));
