02c7e4f46ca36e4dc2cf871aab8d85ad80ff43e7
ginohmk <michotall95@gmail.com>
2023-04-29 12:40:48 +0000

    feat: Add collection , document and insert document

--- mongo_db_basics/server.js
+++ mongo_db_basics/server.js
@@ -14,6 +14,26 @@ const dbConnect = async () => {
     //connect to db
     await client.connect();
     console.log('DB connected Successful!!');
+
+    // Create Db
+    const db = client.db('school');
+
+    // Create collection
+    const studnents = db.collection('students');
+
+    // Create Document
+    const doc = {
+      name: 'Vashaw Shew',
+      email: 'Vashaw@gmail.com',
+      age: 24,
+      major: 'Mathematics',
+    };
+
+    // Send doc to mangodb
+    const result = await studnents.insertOne(doc);
+
+    console.log(result);
+    console.log(studnents);
   } catch (err) {
     console.log(err);
   }
