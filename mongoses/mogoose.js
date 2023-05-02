a19b775507828d1734f4e5f3b76b31713846b1d2
ginohmk <michotall95@gmail.com>
2023-05-02 23:00:20 +0100

    feat: Add schema for student

--- mongoses/mogoose.js
+++ mongoses/mogoose.js
@@ -1,9 +1,22 @@
 const mongoose = require('mongoose');
 
 //connect to mogoose
+
 mongoose
   .connect(
     'mongodb+srv://kanumikechibundu:QOZc1s0sHWmAV7LO@mongodb-demo.roz9auo.mongodb.net/?retryWrites=true&w=majority'
   )
   .then(() => console.log('Db Connnected!!'))
   .catch((err) => console.log(err.message));
+
+// Create a Schema
+
+const studentSchema = mongoose.Schema({
+  name: String,
+  city: String,
+  course: Array,
+  isMarried: Boolean,
+  age: Number,
+});
+
+// Create a model
