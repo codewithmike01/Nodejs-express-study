2bcabb1c7ae296f87a28e6895dfc056e250e4b96
ginohmk <michotall95@gmail.com>
2023-05-05 11:58:46 +0100

    feat: create User schema and colection

---
+++ data-modelling/models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+//  Create Schema
+
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Colection to Schema
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
