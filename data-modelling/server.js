34e616f16a5892d7bb7dca28248b9a65724ae493
ginohmk <michotall95@gmail.com>
2023-05-05 12:16:43 +0100

    feat: Add dbConnection in utils file

--- data-modelling/server.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-// Connecting to server with mongoose
-mongoose
-  .connect(
-    'mongodb+srv://kanumikechibundu:QOZc1s0sHWmAV7LO@mongodb-demo.roz9auo.mongodb.net/school?retryWrites=true&w=majority'
-  )
-  .then((res) => console.log('Connected to Server'))
-  .catch((err) => console.log(err));
