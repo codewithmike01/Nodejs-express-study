f65875a89678e8da2993df3ee2355378e991ab9b
ginohmk <michotall95@gmail.com>
2023-05-14 11:21:10 +0100

    feat: Add user basic auth registration details

---
+++ low-level-authentication/server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+// Connect Db
+mongoose
+  .connect(
+    'mongodb+srv://kanumikechibundu:QOZc1s0sHWmAV7LO@mongodb-demo.roz9auo.mongodb.net/blog-app?retryWrites=true&w=majority'
+  )
+  .then((res) => console.log('Db connected !!!'))
+  .catch((err) => console.log(err));
+
+//Link express
+const app = express();
+
+// Enable json response
+app.use(express.json());
+
+
+
+// Listen on Port
+const PORT = process.env.PORT || '3000';
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
+// User Schema
+const userSchema = mongoose.Schema({
+  fullName: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    default:
+      'https//cdn.pixabay.com/photo/2015/08/05/04/25/people-875617_960_720.jpg',
+  },
+});
+
+// Model
+const User = mongoose.model('User', userSchema);
+
+// Routes
+app.post('/register', async (req, res) => {
+
+  try {
+    const userDetails =  await User.create({
+      fullName: req.body.fullName,
+      username: req.body.username,
+      password: req.body.password,
+    })
+
+    return res.json(userDetails)
+  } catch(err) {
+    return res.json(err)
+  }
+});
